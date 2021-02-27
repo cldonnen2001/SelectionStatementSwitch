ab8a85816a00b55bbd21700003d1343b8dd613ae
Christine Godfrey <cldonnen2001@yahoo.com>
2021-02-26 19:49:02 -0600

    selectoin statement switch exercise

---
+++ SelectionStatementsSwitch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SelectionStatementsSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Tell me what is your favorite class.");
+                string userSubject = Console.ReadLine();
+
+                switch (userSubject.ToLower())
+                {
+                    case "math":
+                        Console.WriteLine("Math - very square");
+                        break;
+                    case "geology":
+                        Console.WriteLine("Geology  - rock N' roll");
+                        break;
+                    case "English":
+                        Console.WriteLine("English - that's poetic");
+                        break;
+                    case "scinece":
+                        Console.WriteLine("Science - how explosive!");
+                        break;
+                    case "world history":
+                        Console.WriteLine("World History - very intriguing");
+                        break;
+                    case "computer science":
+                        Console.WriteLine("Computer Science - arrrggghhhhhh");
+                        break;
+                    default:
+                        Console.WriteLine("Whatever!  Take a real class!");
+                        break;
+                }
+
+            }
+        }
+}
